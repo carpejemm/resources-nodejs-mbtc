08d9e4025d852ebe30f96ece99e744c2aadc1e58
uplift.jem <uplift.jem@gmail.com>
2026-05-12 12:12:50 +0800

    feat: add intro to nodejs and add commands to package json

---
+++ intro-to-nodejs/index.js
@@ -0,0 +1,10 @@
+// print a message to the console
+console.log('Hello from NodeJS!');
+console.log('Hello from NodeJS v2!');
+console.log('Hello from NodeJS! v3!');
+console.log('Hello from NodeJS! v4!');
+
+// print the command line arguments
+console.log('argv:', process.argv);
+
+console.log('Hello from NodeJS, v5!');
