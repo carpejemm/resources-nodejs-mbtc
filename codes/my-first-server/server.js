ebc64139fcb1950d6521e450e2654d09bf995bb4
uplift.jem <uplift.jem@gmail.com>
2026-05-14 16:04:27 +0800

    add dom manipulation codes and my first server in node

---
+++ codes/my-first-server/server.js
@@ -0,0 +1,9 @@
+console.log("Hello world!");
+
+// CommonJS and ES Modules
+// CommonJS - require and module.exports
+// const express = require("express");
+
+
+// ES Modules - import and export
+import express from "express";
